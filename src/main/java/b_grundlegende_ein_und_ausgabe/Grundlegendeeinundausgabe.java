d1810940199493c2390ecef9be536e1a7622c372
codingguide-MG-MR <gergesmarkous@gmail.com>
2024-07-20 14:45:13 +0200

    Anpassung Themen Aufteilung 20.07.2024

--- src/main/java/b_grundlegende_ein_und_ausgabe/Grundlegendeeinundausgabe.java
+++ src/main/java/b_grundlegende_ein_und_ausgabe/Grundlegendeeinundausgabe.java
@@ -1,5 +1,7 @@
 package b_grundlegende_ein_und_ausgabe;
 
+//Hello World, FormattedOutput and Arithmetic, Calculations
+
 public class Grundlegendeeinundausgabe {
     public static void main(String[] args) {
 
